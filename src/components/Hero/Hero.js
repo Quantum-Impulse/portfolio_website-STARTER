fcdc821cc2e958dfb8af7cbea4dfcd8ecd8eb014
Enrique Rivera <erivera7240@gmail.com>
2022-08-17 12:14:34 -0500

    adding stuff to hero and added animation to it

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,23 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Protfolio
+      </SectionTitle>
+
+      <SectionText>
+        I want to become a software engineer, even though I'm a Physics major
+      </SectionText>
+
+      <Button onCLick={() => window.location = 'https://github.com/Quantum-Impulse'}>Resume</Button>
+
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
