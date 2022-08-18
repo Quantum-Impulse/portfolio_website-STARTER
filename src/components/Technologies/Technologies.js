c74d4aed3e5bb38f16c9f877c24537f1e7af7728
Enrique Rivera <erivera7240@gmail.com>
2022-08-18 11:48:09 -0500

    adding svgs to timeline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiDocker, DiReact, DiFirebase } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    
+    <SectionDivider />
+    <br/> <br/>
+    <SectionTitle>Technologies</SectionTitle>
+
+    <SectionText>I've worked with a 
+      range of technologies in the web 
+      development world. From Back-end To Design.
+    </SectionText>
+
+    <List>
+      <ListItem>
+        <DiReact  size="3rem" />
+        <ListContainer>  
+          <ListTitle>Font-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js and Next.js  
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiFirebase  size="3rem" />
+        <ListContainer>  
+          <ListTitle>Backend-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node, MySql, Flask, Nginx, and Express
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem> 
+        <DiDocker  size="3rem" />
+        <ListContainer>  
+          <ListTitle>DevOps</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Github actions (CI, CD, CT), DigitlaOcan, Prometheus Monitoring, and Docker Compose
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+
+  </Section>
 );
 
 export default Technologies;
